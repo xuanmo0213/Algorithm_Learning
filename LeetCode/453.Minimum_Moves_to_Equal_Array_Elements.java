9bf2f7bd8e5321b4d40253fe5a7eb80d6a815ea1
xuanmo0213 <xuanmo0213@gmail.com>
2017-03-19 17:21:06 -0400

    Create solution to LeetCode question 453

---
+++ LeetCode/453.Minimum_Moves_to_Equal_Array_Elements.java
@@ -0,0 +1,30 @@
+/*
+Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
+
+Example:
+
+Input:
+[1,2,3]
+
+Output:
+3
+
+Explanation:
+Only three moves are needed (remember each move increments two elements):
+
+[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+*/
+
+//It's more like a brainstorm, O(n) solution 12 ms, the idea is increment 1 to n - 1 elements equals to extract 1 from max element
+public class Solution {
+
+    public int minMoves(int[] nums) {
+        if (nums.length == 0) return 0;
+        int min = nums[0];
+        for (int n : nums) min = Math.min(min, n);
+        int res = 0;
+        for (int n : nums) res += n - min;
+        return res;
+    }   
+
+}
